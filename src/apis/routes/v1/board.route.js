77a76d370c17c5418592dcf8fe90fd6ade393f81
QuangThai <nguyenquangthaib1@gmail.com>
2022-12-04 22:23:23 +0700

    update authorization route

--- src/apis/routes/v1/board.route.js
+++ src/apis/routes/v1/board.route.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const passport = require('passport')
 
 const { boardController } = require('../../controllers')
 
 const router = express.Router()
 
 router.get('/list', boardController.GetBoards)
-router.post('/new', boardController.CreateBoard)
-router.get('/:id', boardController.GetFullBoard),
-router.get('/safe/:id', boardController.GetFullBoardSafe)
+router.post('/new', passport.authenticate('jwt', { session: false }), boardController.CreateBoard)
+router.get('/:id', boardController.GetFullBoard), router.get('/safe/:id', boardController.GetFullBoardSafe)
 
 module.exports = router
